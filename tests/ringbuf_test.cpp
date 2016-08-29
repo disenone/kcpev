160975bb05fce1542d52a19b3af02d4e08d045c3
disenone <disenonec@gmail.com>
2016-08-29 11:07:49 +0800

    finish ringbuf test

--- tests/ringbuf_test.cpp
+++ tests/ringbuf_test.cpp
@@ -16,7 +16,46 @@ using namespace std;
 
 TEST(RingbufTest, baseTest)
 {
-    ringbuf *rb = ringbuf_new(1);
+    ringbuf *rb = ringbuf_new(5);
+
+    EXPECT_EQ(ringbuf_put(rb, "aaaa", 4), 0);
+
+    EXPECT_EQ(ringbuf_get_pending_size(rb), 4);
+
+    char *data, *data1;
+    EXPECT_EQ(ringbuf_get_next_chunk(rb, &data), 4);
+    EXPECT_EQ(ringbuf_get_next_chunk(rb, &data1), 4);
+    EXPECT_EQ(data, data1);
+
+    ringbuf_mark_consumed(rb, 3);
+    EXPECT_EQ(ringbuf_get_pending_size(rb), 1);
+
+    EXPECT_EQ(ringbuf_get_pending_size(rb), 1);
+
+    EXPECT_EQ(ringbuf_get_next_chunk(rb, &data), 1);
+    EXPECT_EQ(data, data1 + 3);
+
+    EXPECT_EQ(ringbuf_put(rb, "bbbb", 4), 0);
+    EXPECT_EQ(ringbuf_get_pending_size(rb), 5);
+
+    EXPECT_EQ(ringbuf_get_pending_size(rb), 5);
+
+    EXPECT_EQ(ringbuf_get_next_chunk(rb, &data), 2);
+
+    char ret[5];
+    memcpy(ret, data, 2);
+    ringbuf_mark_consumed(rb, 2);
+    EXPECT_EQ(ringbuf_get_pending_size(rb), 3);
+
+    EXPECT_EQ(ringbuf_get_next_chunk(rb, &data), 3);
+    EXPECT_EQ(data, data1);
+
+    memcpy(ret, data, 3);
+    ringbuf_mark_consumed(rb, 3);
+    EXPECT_EQ(ringbuf_get_pending_size(rb), 0);
+
+    EXPECT_EQ(ringbuf_get_next_chunk(rb, &data), 0);
+    EXPECT_EQ(data, (char *)NULL);
 }
 
 int main(int argc, char* argv[])
